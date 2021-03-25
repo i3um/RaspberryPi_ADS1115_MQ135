cc3b78985aa7c188c8fa1246dd5f7d2115d2fd44
Ilya Borisov <47271731+i3um@users.noreply.github.com>
2021-03-25 12:37:58 +0300

    Update values_mq135.py

--- values_mq135.py
+++ values_mq135.py
@@ -1,4 +1,4 @@
-import time, signal, sys, math
+import time, signal, sys, math 
 from Adafruit_ADS1x15 import ADS1x15
 
 
@@ -28,15 +28,12 @@ class MQ135(object):
 
 
     def get_correction_factor(self, temperature, humidity):
-        if temperature < 20:
-            return self.CORA * temperature * temperature - self.CORB * temperature + self.CORC - (humidity - 33.) * self.CORD
-
         return self.CORE * temperature + self.CORF * humidity + self.CORG
 
 
     def get_resistance(self, ad3, pin):
-        a = ad3read_adc(0, 1, 250) / 1000
-        return (1023./a- 1.) * self.RLOAD
+        a = ad3.read_adc(0, 1, 250) / 1000
+        return (1023./a - 1.) * self.RLOAD
 
 
     def get_corrected_resistance(self, temperature, humidity, ad3):
@@ -44,14 +41,14 @@ class MQ135(object):
 
 
     def get_ppm(self, ad3, pin):
-	return self.PARA * math.pow((self.get_resistance(ad3, pin) / self.RZERO), -self.PARB)
+        return self.PARA * math.pow((self.get_resistance(ad3, pin) / self.RZERO), -self.PARB)
 
 
     def get_corrected_ppm(self, temperature, humidity, ad3):
         return self.PARA * math.pow((self.get_corrected_resistance(temperature, humidity, ad3) / self.RZERO), -self.PARB)
 
 
-    def get_rzero(self):
+    def get_rzero(self, ad3):
         return self.get_resistance(ad3, 0) * math.pow((self.ATMOCO2 / self.PARA), (1. / self.PARB))
 
 
@@ -61,20 +58,21 @@ class MQ135(object):
 def mq135lib_example():
     temperature = 21.0
     humidity = 25.0
+
     ad3 = ADS1x15.ADS1115()
     mq135 = MQ135()
-    pin = 0 
-  
+    pin = 0
+
     while (True):
-	rzero = mq135.get_rzero(ad3)
+        rzero = mq135.get_rzero(ad3)
         corrected_rzero = mq135.get_corrected_rzero(temperature, humidity, ad3)
-	resistance = mq135.get_resistance(ad3, pin)
-	ppm = mq135.get_ppm(ad3, pin)
-	corrected_ppm = mq135.get_corrected_ppm(temperature, humidity, ad3)
-	print("MQ135 RZero: " + str(rzero) + "\t Corrected RZero: " + str(corrected_rzero) +
+        resistance = mq135.get_resistance(ad3, pin)
+        ppm = mq135.get_ppm(ad3, pin)
+        corrected_ppm = mq135.get_corrected_ppm(temperature, humidity, ad3)
+        print("MQ135 RZero: " + str(rzero) + "\t Corrected RZero: " + str(corrected_rzero) +
               "\t Resistance: " + str(resistance) + "\t PPM: " + str(ppm) +
-              "\t Corrected PPM: " + str(corrected_ppm) + "ppm")
-	time.sleep(0.3)
+              "\t Corrected PPM: " + str(corrected_ppm) + "ppm", sep = '\n')
+        time.sleep(0.3)
 
 
 if __name__ == "__main__":
